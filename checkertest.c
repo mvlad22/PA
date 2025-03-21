bab2487008a4abcf1a80eb36c0d81d088ec37f34
Vlad <vlad_mihai.munteanu@stud.acs.upb.ro>
2025-03-21 23:10:14 +0200

    task 0 : verificare checker 1

---
+++ checkertest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fin, *fout;
+
+    if((fin=(FILE*)fopen("fisier1.txt","rt"))==NULL)
+    {
+        perror("Reading file error");
+        exit(1);
+    }
+    if((fout=(FILE*)fopen("fisier2.txt","wt"))==NULL)
+    {
+        perror("Writing file error");
+        exit(2);
+    }
+
+    int x;
+
+    fscanf(fin, "%d", &x);
+    fprintf(fout, "Valoarea lui x este %d.\n", x);
+
+    printf("Program executat cu succes.\n");
+
+    fclose(fin);
+    fclose(fout);
+
+    return 0;
+}
