067332c3bdec06be02ef9cbb7110492d03cef059
Vlad <vlad_mihai.munteanu@stud.acs.upb.ro>
2025-03-22 17:27:25 +0200

    task 0: verificare checker cu fisiere .in, .out, .ref

---
+++ Tema_GoL_311AA_MUNTEANU_Vlad-Mihai.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    
+    FILE *fis_in, *fis_out;
+
+    if((fis_in=(FILE*)fopen(argv[1],"rt"))==NULL)
+    {
+        perror("Read file error");
+        exit(1);
+    }
+
+    if((fis_out=(FILE*)fopen(argv[2],"wt"))==NULL)
+    {
+        perror("Write file error");
+        exit(2);
+    }
+
+
+    int nr_task;
+
+    fscanf(fis_in, "%d", &nr_task);
+    fprintf(fis_out, "Numarul task-ului este %d.\n", nr_task);
+
+
+    fclose(fis_in);
+    fclose(fis_out);
+
+    printf("\n\n");
+    return 0;
+}
